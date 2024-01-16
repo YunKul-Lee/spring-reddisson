c9f6d53be916d820c5722f3b21b09b50af224abe
jake <gerion@gmail.com>
2024-01-16 15:23:25 +0900

    Initial commit

---
+++ src/main/java/com/jake/reddisson/SpringReddissonApplication.java
@@ -0,0 +1,13 @@
+package com.jake.reddisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReddissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReddissonApplication.class, args);
+    }
+
+}
